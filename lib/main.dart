a75472e37f12f7f2543b13bf5e04b5307f79f03b
sadatt254 <sadatt254@gmail.com>
2020-08-24 10:47:14 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new Container(
+      decoration:  new BoxDecoration(color: Colors.green),
+      child: new Center(
+        child: new Text("Hello world!")
+      )
+    )
+  )
+}
